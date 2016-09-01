d8b6949866e62bb08ab01865eb5af921433940ab
Spationaute <gabriel.lemieux@usherbrooke.ca>
2016-09-01 14:53:21 -0400

    Remove abug

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@ int main(){
 	Canvas canvas;
 	canvas.setName("test.ppm");
 	canvas.setDimension(640,480);
-	abug	
 	Circle circle_a(320,240,210);
 	circle_a.setColor(0xFFFFFF);
 	canvas.appendShape(&circle_a);
